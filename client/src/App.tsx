b8ae952c7b2071f3e4c196ce8e1bf77103e72c64
Mallikarjun Gudumagatte Nagaraja <mallikarjungn007@gmail.com>
2025-07-01 11:03:52 -0500

    Tell Wouter about the /portfolio base path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,24 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
+import { Router as WouterRouter, Switch, Route } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/toaster";
+import { queryClient } from "./lib/queryClient";
 import Portfolio from "@/pages/Portfolio";
 import NotFound from "@/pages/not-found";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Portfolio} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <WouterRouter base="/portfolio">
+      <QueryClientProvider client={queryClient}>
+        <TooltipProvider>
+          <Toaster />
+          <Switch>
+            <Route path="/" component={Portfolio} />
+            <Route component={NotFound} />
+          </Switch>
+        </TooltipProvider>
+      </QueryClientProvider>
+    </WouterRouter>
   );
 }
 
